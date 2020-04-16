e4d8a9c4197fe50c5232987275b8d1f0a33d8684
simon caceres <kcrs2194@gmail.com>
2020-04-16 12:45:59 -0400

    detalle en main

--- src/main.js
+++ src/main.js
@@ -10,13 +10,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCRaJie9NyiJfGCqKVeIU2PTo8fY5V5fI",
-  authDomain: "calendario-55afa.firebaseapp.com",
-  databaseURL: "https://calendario-55afa.firebaseio.com",
-  projectId: "calendario-55afa",
-  storageBucket: "calendario-55afa.appspot.com",
-  messagingSenderId: "611119583038",
-  appId: "1:611119583038:web:8c81beb30fd9d06ee2a66f"
+  
 };
 // Initialize Firebase
 // Initialize Firebase
